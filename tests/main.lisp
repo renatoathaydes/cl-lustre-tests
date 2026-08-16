38e9f5fc2bca8780f3a01f14f5304a7ddd9d27db
renatoathaydes <renato@athaydes.com>
2026-08-16 14:10:42 +0200

    Made tiny testing framework for testing Lustre itself.

--- tests/main.lisp
+++ tests/main.lisp
@@ -1,18 +1,43 @@
 (in-package #:lustre-tests/tests)
 
-(defun run-tests ()
-  (format T "Running Lustre Tests' own tests!~%")
-  (deftest-defines-test-globally-by-default)
-  (deftest-can-define-test-locally)
-  (format T "Success!~%"))
+;;;; Tiniest test framework in the world ;;;;
 
-(defun deftest-defines-test-globally-by-default ()
+(defparameter *tests* nil)
+
+(defmacro define-lustre-test (name &body body)
+  `(progn
+     (defun ,name () ,@body)
+     (pushnew ',name *tests*)))
+
+;;;; ACTUAL TESTS ;;;;
+
+(define-lustre-test deftest-defines-test-globally-by-default ()
   (let ((lustre-tests::*tests* nil))
     (lustre-tests:deftest my-test () T)
     (assert (= 1 (length lustre-tests::*tests*)))))
 
-(defun deftest-can-define-test-locally ()
+(define-lustre-test deftest-can-define-test-locally ()
   (let ((local-tests nil))
     (lustre-tests:deftest my-test (local-tests) T)
     (assert (= 1 (length local-tests)))
     (assert (= 0 (length lustre-tests::*tests*)))))
+
+;;;; TEST RUNNER ;;;;
+
+(defmacro run-test (name)
+  `(handler-case (progn
+                    (funcall ,name)
+                    (incf success-count))
+     (error (e)
+       (incf error-count)
+       (format T "ERROR: ~A~%  ~A~%" ',name e))))
+
+(defun run-tests ()
+  (format T "Running Lustre Tests' own tests!~%")
+  (let ((error-count 0)
+        (success-count 0))
+    (dolist (test *tests*)
+      (run-test test))
+    (if (zerop error-count)
+        (format T "SUCCESS - all ~A test(s) passed!~%" success-count)
+        (format T "Not ok: ~A error(s), ~A OK.~%" error-count success-count))))
