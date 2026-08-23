5e0bec32c52ee28d3569bf779e0964e52abcffa7
renatoathaydes <renato@athaydes.com>
2026-08-23 20:52:23 +0200

    Writing sequence diff presentation algorithm.

--- src/expects.lisp
+++ src/expects.lisp
@@ -1,8 +1,54 @@
 (in-package #:lustre-tests)
 
-(defparameter *max-diff-matches-to-display* 20
+(defparameter *max-diff-items-to-display* 64
   "The maximum number of individual element matches to display
 when EXPECT-SEQ fails, starting from the first element mismatch.")
+
+(defparameter *max-displayed-items-before-diff* 20
+  "The maximum number of items in a sequence to show before the diff starts")
+
+(defun subvec (seq start end)
+  (coerce (subseq seq start end) 'vector))
+
+(defun fill-shorter (s1 s2)
+  (let ((l1 (file-position s1))
+        (l2 (file-position s2)))
+    (cond
+      ((= l1 l2) nil)
+      ((> l1 l2) (dotimes (n (- l1 l2)) (princ #\SPACE s2)))
+      (T (dotimes (n (- l2 l1)) (princ #\SPACE s1))))))
+
+(defun print-diff (stream expected actual matches prefix expected-suffix actual-suffix diff-index)
+  (let ((index 0)
+        (diff-print-index 0)
+        (es (make-string-output-stream))
+        (as (make-string-output-stream)))
+    ;; loop only to the shorter sequence length
+    (loop for e across expected
+          for a across actual
+          do (progn
+               (when (= index diff-index)
+                 (setf diff-print-index (file-position es)))
+               (format es "~A " e)
+               (format as "~A " a)
+               (fill-shorter es as)
+               (incf index)))
+    ;; now loop to the rest of the longer sequence
+    (multiple-value-bind (seq st)
+        (cond
+          ((> (length actual) (length expected))
+           (values actual as))
+          ((< (length actual) (length expected))
+           (values expected es))
+          (T (values nil nil)))
+      (loop for i from index below (length seq)
+            do (when (= i diff-index)
+                 (setf diff-print-index (file-position st)))
+            do (format st "~A " (aref seq i))))
+    (format stream "Expected: ~A~A~A~%" prefix (get-output-stream-string es) expected-suffix)
+    (format stream "Actual:   ~A~A~A~%" prefix (get-output-stream-string as) actual-suffix)
+    (format stream "          ~v@t~a~%" (+ (length prefix) diff-print-index) "^")
+    (format stream "Changes:  ~A" matches)))
 
 (defun expect-seq (expected actual &optional (test 'equal))
   "Returns NIL is the sequence are equal, or a SIMPLE-TEST-RESULT
@@ -13,20 +59,33 @@ with a failure description otherwise."
         (let* ((first-diff-index (position-if-not
                                   (lambda (m) (eq (car m) :match))
                                   matches))
-               (last-diff-index (min
-                                 (+ first-diff-index *max-diff-matches-to-display*)
-                                 (length matches))))
+               (first-shown-index (max
+                                   0
+                                   (- first-diff-index *max-displayed-items-before-diff*)))
+               (expected-length (length expected))
+               (actual-length (length actual))
+               (max-shown-index (+ first-shown-index *max-diff-items-to-display*))
+               (expected-last-index (min max-shown-index expected-length))
+               (actual-last-index (min max-shown-index actual-length))
+               (last-diff-index (min max-shown-index (max expected-length actual-length)))
+               (prefix (if (> first-shown-index 0) "..." ""))
+               (expected-suffix (if (< expected-last-index expected-length) "..." ""))
+               (actual-suffix (if (< actual-last-index actual-length) "..." "")))
           (make-instance
            'simple-test-result
            :status :failed
            :description (with-output-to-string (s)
-                          (format s "Levenshtein distance: ~D, first diff at ~D~%~A~%"
+                          (format s "Levenshtein distance: ~D, first diff at ~D, showing from ~D to ~D~%"
                                   distance
                                   first-diff-index
-                                  (subseq matches first-diff-index last-diff-index))
-                          (edit-distance:print-diff expected actual
-                                                    :file-stream s
-                                                    :test test
-                                                    :prefix1 "expected"
-                                                    :prefix2 "actual"))))
+                                  first-shown-index
+                                  max-shown-index)
+                          (print-diff s
+                                      (subvec expected first-shown-index expected-last-index)
+                                      (subvec actual first-shown-index actual-last-index)
+                                      (subvec matches first-diff-index last-diff-index)
+                                      prefix
+                                      expected-suffix
+                                      actual-suffix
+                                      (- first-diff-index first-shown-index)))))
         T)))
--- tests/color-sexp-tests/package.lisp
+++ tests/color-sexp-tests/package.lisp
@@ -12,7 +12,7 @@
           (loop for exp in body
                 collect (etypecase exp
                           ((or string character) `(format out "~A" ,exp))
-                          (list `(ansi:format-ansi out (list ',exp)))))))
+                          (list `(ansi:format-ansi out (list (list ,@exp))))))))
     `(with-output-to-string (out) ,@transformed-body)))
 
 (defpackage my-symbols
@@ -20,10 +20,89 @@
 
 (lt:define-test colors-symbol (color-sexp)
   (lt:expect-seq
-   (ansi-seq "(" (:fg :cyan "MY-SYMBOLS:FOO") ")")
+   (ansi-seq "(" (:fg cs:+default-symbol-color+ "MY-SYMBOLS:FOO") ")")
    (cs:color-sexp-to-string '(my-symbols:foo))))
 
-(lt:define-test temp-test (color-sexp)
+(lt:define-test colors-numbers (color-sexp)
   (lt:expect-seq
-   '(1 2 3)
-   '(1 2 3)))
+   (ansi-seq "(" (:fg cs:+default-number-color+ "10") ")")
+   (cs:color-sexp-to-string '(10))))
+
+(lt:define-test colors-keywords (color-sexp)
+  (lt:expect-seq
+   (ansi-seq "(" (:fg cs:+default-keyword-color+ ":HELLO") ")")
+   (cs:color-sexp-to-string '(:hello))))
+
+(lt:define-test colors-special-operators (color-sexp)
+  (lt:expect-seq
+   (ansi-seq "(" (:fg cs:+default-special-operator-color+ "FUNCTION") ")")
+   (cs:color-sexp-to-string '(function))))
+
+(lt:define-test colors-chars-and-strings (color-sexp)
+  (lt:expect-seq
+   (ansi-seq "("
+             (:fg cs:+default-char-color+ "#\\A")
+             " "
+             (:fg cs:+default-string-color+ "\"Joe\"")
+             ")")
+   (cs:color-sexp-to-string '(#\A "Joe"))))
+
+(lt:define-test colors-standard-macros (color-sexp)
+  (lt:expect-seq
+   (ansi-seq "(" (:fg cs:+default-standard-macro-color+ "DEFUN") ")")
+   (cs:color-sexp-to-string '(defun))))
+
+(lt:define-test colors-lambda-list-keywords (color-sexp)
+  (lt:expect-seq
+   (ansi-seq "(" (:fg cs:+default-lambda-list-keyword-color+ "&OPTIONAL") ")")
+   (cs:color-sexp-to-string '(&optional))))
+
+(lt:define-test colors-full-expression (color-sexp)
+  (lt:expect-seq
+   (ansi-seq "("
+             (:fg cs:+default-standard-macro-color+ "DEFUN")
+             " "
+             (:fg cs:+default-symbol-color+ ":MY-FUN")
+             " ("
+             (:fg cs:+default-symbol-color+ "ARG")
+             " "
+             (:fg cs:+default-lambda-list-keyword-color+ "&KEY")
+             " "
+             (:fg cs:+default-symbol-color+ "K1")
+             ") "
+             (:fg cs:+default-string-color+ "\"My function.\"")
+             " ("
+             (:fg cs:+default-special-operator-color+ "LET")
+             " (("
+             (:fg cs:+default-symbol-color+ "X")
+             " "
+             (:fg cs:+default-char-color+ "#\\A")
+             ")) ("
+             (:fg cs:+default-standard-macro-color+ "DOLIST")
+             " ("
+             (:fg cs:+default-symbol-color+ "ITEM")
+             " "
+             (:fg cs:+default-symbol-color+ "K1")
+             ") ("
+             (:fg cs:+default-symbol-color+ "FORMAT")
+             " "
+             (:fg cs:+default-symbol-color+ "T")
+             " "
+             (:fg cs:+default-symbol-color+ "STR")
+             " "
+             (:fg cs:+default-symbol-color+ "ARG")
+             " ("             
+             (:fg cs:+default-symbol-color+ "Y")
+             " "
+             (:fg cs:+default-number-color+ "2")
+             " "
+             (:fg cs:+default-symbol-color+ "X")
+             " "
+             (:fg cs:+default-symbol-color+ "ITEM")
+             ")))))")
+   (cs:color-sexp-to-string
+    '(defun :my-fun (arg &key k1)
+      "My function."
+      (let ((x #\A))
+        (dolist (item k1) (format T "STR" arg (y 2 x item))))))))
+  
