1f890aae183b4cb352fa0e150a1abe94ae8de2cf
renatoathaydes <renato@athaydes.com>
2026-08-29 13:47:53 +0200

    Updated expect-seq tests after refactoring, fixed more bugs.

--- src/expects.lisp
+++ src/expects.lisp
@@ -58,30 +58,38 @@ when EXPECT-SEQ fails, starting from the first element mismatch.")
 (defun print-diff (stream strings? matches prefix
                    expected-suffix actual-suffix)
   (let ((ansi? *show-diff-with-ansi-colors*)
+        (len (length matches))
         (es (make-instance 'expects-output-stream))
         (as (make-instance 'expects-output-stream))
         (ms (make-instance 'expects-output-stream)))
-    (labels ((print-item (item stream)
+    (labels ((print-item (item last? stream)
                (if strings?
                    (write-char item stream)
                    (format stream "~A" item))
-               (unless (and strings? ansi?)
+               (unless (or last? (and strings? ansi?))
                  (write-char #\SPACE stream))))
-      (loop for m across matches
+      (loop for i from 0 below len
+            for m = (aref matches i)
+            for last? = (= (1+ i) len)
+            ;; each match has the form:
+            ;;   (:match expected actual)
+            ;;   (:substitution expected actual)
+            ;;   (:insertion nil actual)
+            ;;   (:deletion expected nil)
             do (ecase (first m)
                  (:match
-                     (print-item (second m) es) (print-item (third m) as))
+                     (print-item (second m) last? es) (print-item (third m) last? as))
                  (:substitution
                   (with-color (when ansi? :yellow) (es as)
-                    (print-item (second m) es) (print-item (third m) as))
+                    (print-item (second m) last? es) (print-item (third m) last? as))
                   (unless ansi? (princ "~" ms)))
                  (:insertion
                   (with-color (when ansi? :green) (as)
-                    (print-item (third m) as))
+                    (print-item (third m) last? as))
                   (unless ansi? (princ "+" ms)))
                  (:deletion
                   (with-color (when ansi? :red) (es)
-                    (print-item (second m) es))
+                    (print-item (second m) last? es))
                   (unless ansi? (princ "-" ms))))
             do (sync-visible-positions es as ms))
       (when ansi? (ansi::print-ansi :fg :reset stream))
@@ -110,12 +118,12 @@ with a failure description otherwise."
                                    (- first-diff-index *max-displayed-items-before-diff*)))
                (expected-length (length expected))
                (actual-length (length actual))
-               (max-shown-index (+ first-shown-index *max-diff-items-to-display*))
-               (expected-last-index (min max-shown-index expected-length))
-               (actual-last-index (min max-shown-index actual-length))
+               (max-shown-index (+ first-diff-index (1- *max-diff-items-to-display*)))
+               (expected-last-index (min max-shown-index (1- expected-length)))
+               (actual-last-index (min max-shown-index (1- actual-length)))
                (prefix (if (> first-shown-index 0) "..." ""))
-               (expected-suffix (if (< expected-last-index expected-length) "..." ""))
-               (actual-suffix (if (< actual-last-index actual-length) "..." "")))
+               (expected-suffix (if (< expected-last-index (1- expected-length)) "..." ""))
+               (actual-suffix (if (< actual-last-index (1- actual-length)) "..." "")))
           (make-instance
            'simple-test-result
            :status :failed
--- tests/tests/expect-seq-tests.lisp
+++ tests/tests/expect-seq-tests.lisp
@@ -12,152 +12,162 @@
   (assert-ok-result (lt:expect-seq "" "")))
 
 (define-lustre-test expect-seq-not-equal-same-length
-  (let ((lustre-tests::*max-diff-items-to-display* 3)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq "a" "b")
-     "Levenshtein distance: 1, first diff at 0, showing from 0 to 1
+     "Levenshtein distance: 1, first diff at 0, showing from 0 to 0
 Expected: a
 Actual:   b
           ~")))
 
 (define-lustre-test expect-seq-not-equal-first-shorter
-  (let ((lustre-tests::*max-diff-items-to-display* 3)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq "a" "ab")
-     "Levenshtein distance: 1, first diff at 1, showing from 0 to 2
-Expected: a
+     "Levenshtein distance: 1, first diff at 1, showing from 0 to 1
+Expected: a  
 Actual:   a b
             +")))
 
 (define-lustre-test expect-seq-not-equal-first-longer
-  (let ((lustre-tests::*max-diff-items-to-display* 3)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq "ab" "a")
-     "Levenshtein distance: 1, first diff at 1, showing from 0 to 2
+     "Levenshtein distance: 1, first diff at 1, showing from 0 to 1
 Expected: a b
-Actual:   a
+Actual:   a  
             -")))
 
 (define-lustre-test expect-seq-not-equal-same-length-too-long-to-show-start
-  (let ((lustre-tests::*max-diff-items-to-display* 4)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 4)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq "abcde" "abcfe")
-     "Levenshtein distance: 1, first diff at 3, showing from 1 to 5
+     "Levenshtein distance: 1, first diff at 3, showing from 1 to 4
 Expected: ...b c d e
 Actual:   ...b c f e
-                 ~ ")))
+                 ~  ")))
 
 (define-lustre-test expect-seq-not-equal-same-length-too-long-to-show-end
-  (let ((lustre-tests::*max-diff-items-to-display* 4)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
+    (assert-failed-description
+     (lustre-tests:expect-seq "abcde" "axcde")
+     "Levenshtein distance: 1, first diff at 1, showing from 0 to 3
+Expected: a b c d...
+Actual:   a x c d...
+            ~    ")))
+
+(define-lustre-test expect-seq-not-equal-same-length-show-end-exactly
+  (let ((lustre-tests:*max-diff-items-to-display* 4)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq "abcde" "axcde")
      "Levenshtein distance: 1, first diff at 1, showing from 0 to 4
-Expected: a b c d...
-Actual:   a x c d...
-            ~   ")))
+Expected: a b c d e
+Actual:   a x c d e
+            ~      ")))
 
 (define-lustre-test expect-seq-not-equal-same-length-too-long-to-show-start-end
-  (let ((lustre-tests::*max-diff-items-to-display* 4)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 2)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
-     (lustre-tests:expect-seq "abcdefgh" "abcdxfgh")
-     "Levenshtein distance: 1, first diff at 4, showing from 2 to 6
+     (lustre-tests:expect-seq "abcdefg" "abcdxfg")
+     "Levenshtein distance: 1, first diff at 4, showing from 2 to 5
 Expected: ...c d e f...
 Actual:   ...c d x f...
-                 ~ ")))
+                 ~  ")))
 
 (define-lustre-test expect-seq-not-equal-deletions-and-substitutions
-  (let ((lustre-tests::*max-diff-items-to-display* 5)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq "abcdefgh" "abcdfxh")
-     "Levenshtein distance: 2, first diff at 4, showing from 2 to 7
+     "Levenshtein distance: 2, first diff at 4, showing from 2 to 6
 Expected: ...c d e f g...
-Actual:   ...c d   f x 
+Actual:   ...c d   f x
                  -   ~")))
 
 (define-lustre-test expect-seq-not-equal-includes-invisible-characters
-  (let ((lustre-tests::*max-diff-items-to-display* 6)
-        (lustre-tests::*max-displayed-items-before-diff* 3))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 3))
     (flet ((conc (s1 char s2)
              (concatenate 'string s1 (string char) s2)))
       (assert-failed-description
        (lustre-tests:expect-seq (conc "abcd" #\ESC "efgh") (conc "abcd" #\ESC "fxhi"))
-       "Levenshtein distance: 3, first diff at 5, showing from 2 to 8
+       "Levenshtein distance: 3, first diff at 5, showing from 2 to 7
 Expected: ...c d \\x1B e f g...
-Actual:   ...c d \\x1B   f x ...
+Actual:   ...c d \\x1B   f x...
                       -   ~"))))
 
 ;;; SAME TESTS REPEATED FOR INTEGER SEQUENCES WITH MEMBERS HAVING MORE THAN ONE DIGIT
 
 (define-lustre-test expect-seq-not-equal-same-length-ints
-  (let ((lustre-tests::*max-diff-items-to-display* 3)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq #(10) #(11))
-     "Levenshtein distance: 1, first diff at 0, showing from 0 to 1
+     "Levenshtein distance: 1, first diff at 0, showing from 0 to 0
 Expected: 10
 Actual:   11
-          ~")))
+          ~ ")))
 
 (define-lustre-test expect-seq-not-equal-first-shorter-ints
-  (let ((lustre-tests::*max-diff-items-to-display* 3)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq #(10) #(10 11))
-     "Levenshtein distance: 1, first diff at 1, showing from 0 to 2
-Expected: 10
+     "Levenshtein distance: 1, first diff at 1, showing from 0 to 1
+Expected: 10   
 Actual:   10 11
-             +")))
+             + ")))
 
 (define-lustre-test expect-seq-not-equal-first-longer-ints
-  (let ((lustre-tests::*max-diff-items-to-display* 3)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq #(10 11) #(10))
-     "Levenshtein distance: 1, first diff at 1, showing from 0 to 2
+     "Levenshtein distance: 1, first diff at 1, showing from 0 to 1
 Expected: 10 11
-Actual:   10
-             -")))
+Actual:   10   
+             - ")))
 
 (define-lustre-test expect-seq-not-equal-same-length-too-long-to-show-start-ints
-  (let ((lustre-tests::*max-diff-items-to-display* 4)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 2)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq #(10 11 12 13 14) #(10 11 12 66 14))
-     "Levenshtein distance: 1, first diff at 3, showing from 1 to 5
+     "Levenshtein distance: 1, first diff at 3, showing from 1 to 4
 Expected: ...11 12 13 14
 Actual:   ...11 12 66 14
-                   ~  ")))
+                   ~    ")))
 
 (define-lustre-test expect-seq-not-equal-same-length-too-long-to-show-end-ints
-  (let ((lustre-tests::*max-diff-items-to-display* 4)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 3)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq #(10 11 12 13 14) #(10 66 12 13 14))
-     "Levenshtein distance: 1, first diff at 1, showing from 0 to 4
+     "Levenshtein distance: 1, first diff at 1, showing from 0 to 3
 Expected: 10 11 12 13...
 Actual:   10 66 12 13...
-             ~     ")))
+             ~       ")))
 
 (define-lustre-test expect-seq-not-equal-same-length-too-long-to-show-start-end-ints
-  (let ((lustre-tests::*max-diff-items-to-display* 4)
-        (lustre-tests::*max-displayed-items-before-diff* 2))
+  (let ((lustre-tests:*max-diff-items-to-display* 2)
+        (lustre-tests:*max-displayed-items-before-diff* 2))
     (assert-failed-description
      (lustre-tests:expect-seq #(10 11 12 13 14 15 16 17) '(10 11 12 13 66 15 16 17))
-     "Levenshtein distance: 1, first diff at 4, showing from 2 to 6
+     "Levenshtein distance: 1, first diff at 4, showing from 2 to 5
 Expected: ...12 13 14 15...
 Actual:   ...12 13 66 15...
-                   ~  ")))
+                   ~    ")))
 
 (define-lustre-test expect-seq-diff-with-ansi-colors
-  (let ((lustre-tests::*max-diff-items-to-display* 24)
-        (lustre-tests::*max-displayed-items-before-diff* 4)
+  (let ((lustre-tests:*max-diff-items-to-display* 24)
+        (lustre-tests:*max-displayed-items-before-diff* 4)
         (lustre-tests:*show-diff-with-ansi-colors* T))
     (macrolet ((print-colored (color text stream)
                  `(progn
@@ -167,15 +177,15 @@ Actual:   ...12 13 66 15...
       (assert-failed-description
        (lustre-tests:expect-seq #(10 11 12 13 14 15 16 17 18) '(10 11 12 13 15 16 77 18 19 20))
        (with-output-to-string (s)
-         (format s "~A~%" "Levenshtein distance: 4, first diff at 4, showing from 0 to 10")
+         (format s "~A~%" "Levenshtein distance: 4, first diff at 4, showing from 0 to 9")
          (ansi::print-ansi :fg :reset s)
          (write-string "Expected: 10 11 12 13 " s)
          (print-colored :red "14 " s)
          (write-string "15 16 " s)
          (print-colored :yellow "17 " s)
-         (write-string "18       " s)
+         (write-string "18      " s)
          (format s "~%~A" "Actual:   10 11 12 13    15 16 ")
          (print-colored :yellow "77 " s)
          (write-string "18 " s)
          (print-colored :green "19 " s)
-         (print-colored :green "20 " s))))))
+         (print-colored :green "20" s))))))
