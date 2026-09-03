63dcd10bba9d97acb66089709b8efebb6bc68abd
renatoathaydes <renato@athaydes.com>
2026-09-03 19:48:12 +0200

    Test end-to-end simple-test-reporter.

--- tests/tests/end-to-end-tests.lisp
+++ tests/tests/end-to-end-tests.lisp
@@ -10,6 +10,27 @@
      (unwind-protect (progn ,@body)
        (setf (symbol-function 'lustre-tests/time:print-time) original-fn))))
 
+(define-lustre-test report-successful-tests-correctly-simple
+  (with-local-root (root)
+    (in-package #:lustre-tests/tests) ;; fixes symbols for the test names below!
+    (lt:define-test test-2+2=4 ()
+      (assert (= (+ 2 2) 4)))
+    (lt:define-test test-string (string-tests)
+      (assert (string= (string #\A) "A")))
+    (let ((result
+            (with-output-to-string (stream)
+              (mock-time
+                (lt:test-simple :stream stream :test-parent root :parallel? nil)))))
+      (lt:expect-seq "== LUSTRE TESTS ==
+Running 2 test(s).
+OK: LUSTRE-TESTS/TESTS::TEST-2+2=4 (1)
+  >> STRING-TESTS
+  OK: LUSTRE-TESTS/TESTS::TEST-STRING (2)
+  << STRING-TESTS (3)
+Success: 2, Ignored: 0, Failures: 0 (4)
+"
+                     result))))
+
 (define-lustre-test report-successful-tests-correctly-by-default
   (with-local-root (root)
     (in-package #:lustre-tests/tests) ;; fixes symbols for the test names below!
